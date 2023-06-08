bbf52bd7b11c0ed2d214f22b6025809d4f6d0724
smul0002 <130717893+smul0002@users.noreply.github.com>
2023-06-08 20:11:58 +0930

    Update old_accounts.R

--- old_accounts.R
+++ old_accounts.R
@@ -6,6 +6,7 @@ install.packages("ggplot2")
 library(tidyverse)
 library(ggplot2)
 library(mongolite)
+library(dplyr)
 
 
 connection_string = 'mongodb+srv://smul0002:2V9NiiU6spHC2Gkz@cluster0.tfzpkuq.mongodb.net/?retryWrites=true&w=majority'
@@ -65,5 +66,61 @@ last_transactions[3,2] - accounts_last[2,]
 diff_dates = difftime(final_transaction,last_transactions[1,2], units = "days")
 diff_dates
 
+#to clear pie and data for testing plots.
+pie <- c()
+data <- c()
 
+
+#Pie chart:
+
+pie <- rbind(count(alive_accounts), count(dead_accounts))
+
+# Add Names
+data <- data.frame(
+  Accounts=c('Alive Accounts:','Dead Accounts:'),
+  value=pie
+)
+
+# Compute the position of labels
+data <- data %>% 
+  arrange(desc(Accounts)) %>%
+  mutate(prop = n / sum(data$n) *100) %>%
+  mutate(ypos = cumsum(prop)- 0.5*prop )
+
+# Piechart
+ggplot(data, aes(x="", y=prop, fill=Accounts)) +
+  geom_bar(stat="identity", width=1, color="white") +
+  coord_polar("y", start=0) +
+  theme_void() + 
+  theme(legend.position="none") +
+  
+  geom_text(aes(y = ypos, label = Accounts), color = "white", size=4) +
+  scale_fill_brewer(palette="Set1")
+
+
+
+#Donut Chart:
+# Compute percentages
+data$fraction <- data$n / sum(data$n)
+
+# Compute the cumulative percentages (top of each rectangle)
+data$ymax <- cumsum(data$fraction)
+
+# Compute the bottom of each rectangle
+data$ymin <- c(0, head(data$ymax, n=-1))
+
+# Compute label position
+data$labelPosition <- (data$ymax + data$ymin) / 2
+
+# Compute a good label
+data$label <- paste0(data$Accounts, "\n", data$n)
+
+# Make the plot
+ggplot(data, aes(ymax=ymax, ymin=ymin, xmax=4, xmin=3, fill=Accounts)) +
+  geom_rect() +
+  geom_text( x=.6, aes(y=labelPosition, label=label, color=Accounts), size=7) +
+  coord_polar(theta="y") +
+  xlim(c(-1, 4)) +
+  theme_void() +
+  theme(legend.position = "none")
 
