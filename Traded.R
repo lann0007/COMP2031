feee6c08e59e2ec868117466190e70eec0263a04
smul0002 <130717893+smul0002@users.noreply.github.com>
2023-06-08 21:40:21 +0930

    Update Traded.R

--- Traded.R
+++ Traded.R
@@ -9,7 +9,10 @@ library(mongolite)
 
 
 connection_string = 'mongodb+srv://smul0002:2V9NiiU6spHC2Gkz@cluster0.tfzpkuq.mongodb.net/?retryWrites=true&w=majority'
+
 transactions_collection = mongo(collection = "transactions", db = "sample_analytics", url = connection_string)
+
+
 
 transaction_types = transactions_collection$aggregate('[{"$project": {"transactions": 1}}]')
 
@@ -19,6 +22,7 @@ df <- as.data.frame(transaction_types)
 list <- c()
 for (i in 1:nrow(df)) {
   temp <- as.data.frame(df[[2]][[i]])
+  
   list <- rbind(list,(temp[,2:4]))
 }
 
@@ -33,9 +37,43 @@ table(sell$symbol)
 table(list$symbol)
 
 
-#sill working on this
+#Times Bought and sold
 times_bought <- buy %>% count(symbol)
-times_sold <- buy %>% count(symbol)
+times_sold <- sell %>% count(symbol)
 
+#Quantity of each symbol sold
+Total_Quantity_Buy <- aggregate(amount ~ symbol, data=buy, sum)
+Total_Quantity_Sell <- aggregate(amount ~ symbol, data=sell, sum)
+sum(buy$amount)
+sum(sell$amount)
+
+
+#Bar Charts
 ggplot(times_bought, aes(x=symbol, y=n))+
   geom_bar(stat = "identity")
+
+ggplot(times_sold, aes(x=symbol, y=n))+
+  geom_bar(stat = "identity")
+
+#Point Plots
+ggplot(times_bought, aes(x=symbol, y=n))+
+  geom_point(stat = "identity")
+
+ggplot(times_sold, aes(x=symbol, y=n))+
+  geom_point(stat = "identity")
+
+
+# Buy piechart
+ggplot(times_bought, aes(x="", y=n, fill=symbol)) +
+  geom_bar(stat="identity", width=1, color="white") +
+  coord_polar("y", start=0) +
+  
+  theme_void() # remove background, grid, numeric labels
+
+# Sell piechart
+ggplot(times_bought, aes(x="", y=n, fill=symbol)) +
+  geom_bar(stat="identity", width=1, color="white") +
+  coord_polar("y", start=0) +
+  
+  theme_void() # remove background, grid, numeric labels
+
