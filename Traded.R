290b4c1847210eee9182b41289f6a32decd24d4b
smul0002 <130717893+smul0002@users.noreply.github.com>
2023-06-10 17:03:33 +0930

    Update Traded.R

--- Traded.R
+++ Traded.R
@@ -40,6 +40,33 @@ table(list$symbol)
 #Times Bought and sold
 times_bought <- buy %>% count(symbol)
 times_sold <- sell %>% count(symbol)
+#add name
+times_bought$type <- "buy"
+times_bought <- times_bought[c(3, 1, 2)]
+times_sold$type <- "sold"
+times_sold <- times_sold[c(3, 1, 2)]
+
+#combine times_bought and times_sold, then rename n to Count
+transaction_count <- rbind(times_bought,times_sold)
+
+colnames(transaction_count)[3] ="Count"
+
+
+#create chart showing times bought and sold on same graph
+ggplot(transaction_count, aes(symbol, Count, fill = type)) + 
+  geom_bar(stat="identity", position = "dodge") + 
+  scale_fill_brewer(palette = "Set1")
+
+
+
+
+#total transactions
+sum(transaction_count$Count)
+sum(times_bought$n)
+sum(times_sold$n)
+
+
+
 
 #Quantity of each symbol sold
 Total_Quantity_Buy <- aggregate(amount ~ symbol, data=buy, sum)
@@ -47,33 +74,41 @@ Total_Quantity_Sell <- aggregate(amount ~ symbol, data=sell, sum)
 sum(buy$amount)
 sum(sell$amount)
 
+#Same as above but for quantity
+Total_Quantity_Buy$type <- "bought_amount"
+Total_Quantity_Buy <- Total_Quantity_Buy[c(3, 1, 2)]
+Total_Quantity_Sell$type <- "sold_amount"
+Total_Quantity_Sell <- Total_Quantity_Sell[c(3, 1, 2)]
 
-#Bar Charts
-ggplot(times_bought, aes(x=symbol, y=n))+
-  geom_bar(stat = "identity")
+Total_Quantity <- rbind(Total_Quantity_Buy,Total_Quantity_Sell)
 
-ggplot(times_sold, aes(x=symbol, y=n))+
-  geom_bar(stat = "identity")
-
-#Point Plots
-ggplot(times_bought, aes(x=symbol, y=n))+
-  geom_point(stat = "identity")
-
-ggplot(times_sold, aes(x=symbol, y=n))+
-  geom_point(stat = "identity")
+#graph
+ggplot(Total_Quantity, aes(symbol, amount, fill = type)) +
+  geom_bar(stat="identity", position = "dodge") +
+  scale_fill_brewer(palette = "Set1")
 
 
-# Buy piechart
-ggplot(times_bought, aes(x="", y=n, fill=symbol)) +
-  geom_bar(stat="identity", width=1, color="white") +
-  coord_polar("y", start=0) +
-  
-  theme_void() # remove background, grid, numeric labels
 
-# Sell piechart
-ggplot(times_bought, aes(x="", y=n, fill=symbol)) +
-  geom_bar(stat="identity", width=1, color="white") +
-  coord_polar("y", start=0) +
-  
-  theme_void() # remove background, grid, numeric labels
+#total Quantity
+sum(Total_Quantity$amount)
+sum(Total_Quantity_Buy$amount)
+sum(Total_Quantity_Sell$amount)
+
+test <- data.frame(
+  Type=c("Total Quantity Bought", "Total Quantity Sold"),
+  count=c(sum(Total_Quantity_Buy$amount),sum(Total_Quantity_Sell$amount))
+)
+
+rownames(test) <- c("219785213","220042344")
+
+ggplot(test, aes(x=Type, y=count))+
+  geom_point(stat = "identity")+
+  ylim(219700000, 220050000)+
+  geom_text(
+    label=rownames(test), 
+    nudge_x = 0.25, nudge_y = 0.25, 
+    check_overlap = T)
+
+
+
 
